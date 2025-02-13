0c0b2decd39d64249b167e39c2fcca41f72a52ee
JBernal <j.bernal@upm.es>
2025-02-13 17:24:00 +0100

    jpa is ok

--- betca-jpa/src/main/java/es/upm/miw/betca_jpa/entities/AnyClass.java
+++ betca-jpa/src/main/java/es/upm/miw/betca_jpa/entities/AnyClass.java
@@ -4,12 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serial;
 import java.io.Serializable;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class AnyClass implements Serializable {
+    @Serial
     private static final long serialVersionUID = 1L;
     private Integer number;
     private String name;
